29d3e338cba48b0db5ec0824b154cd8428e7b9e6
Ricardo Tejada <132228164+RikrdoTejada@users.noreply.github.com>
2025-10-11 14:03:13 -0500

    Revert "Feature/post general : CRUD para publicacion en foro general con validacion"

--- src/main/java/com/impulsofit/model/Grupo.java
+++ src/main/java/com/impulsofit/model/Grupo.java
@@ -1,38 +1,4 @@
 package com.impulsofit.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "grupo")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class Grupo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_grupo;
-
-    @ManyToOne
-    @JoinColumn(name = "id_usuario_creador", nullable = false)
-    private Usuario usuario_creador;
-
-    @ManyToOne
-    @JoinColumn(name = "id_deporte", nullable = false)
-    private Deporte deporte;
-
-    @Column
-    private String nombre;
-    @Column
-    private String descripcion;
-    @Column
-    private String ubicacion;
-    @Column
-    private LocalDate fecha_creacion;
-
 }
--- src/main/java/com/impulsofit/repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.impulsofit.repository;
-import com.impulsofit.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
